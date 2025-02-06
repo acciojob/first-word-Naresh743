a729a732978323647bd992093e0379828d07c4c7
Naresh743 <nareshdevra743@gmail.com>
2025-02-06 11:48:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+  let index = s.indexOf(' ');
+
+  // If there is no space, return the whole string; otherwise, return up to the first space
+  return index === -1 ? s : s.substring(0, index);
 }
 
 // Do not change the code below
